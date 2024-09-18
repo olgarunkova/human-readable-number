71b810b4b513a758a7dd240979f06a7484a26e17
Olga <171145876+olgarunkova@users.noreply.github.com>
2024-09-18 11:39:05 -0700

    Add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function toReadable (number) {
+  const digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  const teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+  let readable = "";
+
+  if (number === 0) return digits[0];
+
+  // Hundreads
+  if (number >= 100) {
+    readable = digits[Math.floor(number / 100)] + " hundred";
+    let remainder = number % 100;
+    if (remainder === 0) return readable.trim();
+    if (remainder < 10) return readable = readable + " " + digits[remainder];
+    if (remainder >= 10 && remainder < 20) return readable += " " + teens[remainder -10];
+    if (remainder % 10 === 0) return readable += " " + tens[Math.floor(remainder/10)];
+    if (remainder >= 20) return readable += " " + tens[Math.floor(remainder/10)] + " " + digits[remainder % 10];
+  }
+
+  // Tens 
+  if ( number > 19 && number < 100) {
+    readable = tens[Math.floor(number / 10)];
+    if (number % 10 !== 0) return readable += " " + digits[number % 10];
+    return readable.trim();
+  }
+
+  // Teens 
+  if ( number > 9 && number < 20) {
+    return readable += teens[number - 10];
+  }
+
+  // Digits 
+  if ( number < 10) {
+     return readable += digits[number];
+  }
   
 }
